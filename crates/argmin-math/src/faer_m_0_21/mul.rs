179069d7ccbdc2ab910cdc0b399ab0652984eeb0
Fabian Weik <fabian.weik@protonmail.com>
2025-12-18 10:57:47 +0100

    add missing trait implementations for multiplying col with scalar values

--- crates/argmin-math/src/faer_m_0_21/mul.rs
+++ crates/argmin-math/src/faer_m_0_21/mul.rs
@@ -106,3 +106,39 @@ impl<E: ComplexField> ArgminMul<Col<E>, Col<E>> for Col<E> {
         <_ as ArgminMul<_, _>>::mul(&self.as_ref(), &other.as_ref())
     }
 }
+
+/// E * ColRef -> Col
+impl<E: ComplexField> ArgminMul<ColRef<'_, E>, Col<E>> for E {
+    #[inline]
+    fn mul(&self, other: &ColRef<'_, E>) -> Col<E> {
+        let mut result = Col::zeros(other.nrows()); // TODO: why not possible like sub
+        zip!(&mut result, other).for_each(|unzip!(result, other)| *result = self.mul_by_ref(other));
+        result
+    }
+}
+
+/// E * Col -> Col
+impl<E: ComplexField> ArgminMul<Col<E>, Col<E>> for E {
+    #[inline]
+    fn mul(&self, other: &Col<E>) -> Col<E> {
+        <_ as ArgminMul<_, _>>::mul(self, &other.as_ref())
+    }
+}
+
+/// ColRef * E -> Col
+impl<E: ComplexField> ArgminMul<E, Col<E>> for ColRef<'_, E> {
+    #[inline]
+    fn mul(&self, other: &E) -> Col<E> {
+        let mut result = Col::zeros(self.nrows()); // TODO: why not possible like sub
+        zip!(&mut result, self).for_each(|unzip!(result, self_)| *result = self_.mul_by_ref(other));
+        result
+    }
+}
+
+/// Col * E -> Col
+impl<E: ComplexField> ArgminMul<E, Col<E>> for Col<E> {
+    #[inline]
+    fn mul(&self, other: &E) -> Col<E> {
+        <_ as ArgminMul<_, _>>::mul(&self.as_ref(), other)
+    }
+}
