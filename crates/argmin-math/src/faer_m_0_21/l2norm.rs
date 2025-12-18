e17074d20cd05d3da2532892936bfe0075155aea
Fabian Weik <fabian.weik@protonmail.com>
2025-12-18 10:03:32 +0100

    implement some traits needed for solver

--- crates/argmin-math/src/faer_m_0_21/l2norm.rs
+++ crates/argmin-math/src/faer_m_0_21/l2norm.rs
@@ -1,5 +1,5 @@
 use crate::ArgminL2Norm;
-use faer::{Mat, MatRef};
+use faer::{Col, ColRef, Mat, MatRef};
 use faer_traits::ComplexField;
 
 impl<E: ComplexField> ArgminL2Norm<E::Real> for MatRef<'_, E> {
@@ -9,6 +9,18 @@ impl<E: ComplexField> ArgminL2Norm<E::Real> for MatRef<'_, E> {
 }
 
 impl<E: ComplexField> ArgminL2Norm<E::Real> for Mat<E> {
+    fn l2_norm(&self) -> E::Real {
+        self.norm_l2()
+    }
+}
+
+impl<E: ComplexField> ArgminL2Norm<E::Real> for ColRef<'_, E> {
+    fn l2_norm(&self) -> E::Real {
+        self.norm_l2()
+    }
+}
+
+impl<E: ComplexField> ArgminL2Norm<E::Real> for Col<E> {
     fn l2_norm(&self) -> E::Real {
         self.norm_l2()
     }
